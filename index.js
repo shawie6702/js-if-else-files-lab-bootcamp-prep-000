8ac568773b0eb8649fed079dccf89dc2592c122d
swhitesel@comcast.net <swhitesel@comcast.net>
2018-09-09 12:29:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ if (name === "Susan") {console.log("The name is correct")}
 else {
   console.log("Expected the name to be Susan")
 }
+<<<<<<< HEAD
 
 if (height === "74") {
     console.log("The height is correct")
 } else {
     console.log("Expected the height to be 74")
 }
+=======
+>>>>>>> 41ca36d0ce68b1ce9698c0c3f1b3f8206f35225c
 
 
 // Don't worry about this
--- other_file.js
+++ other_file.js
@@ -2,4 +2,11 @@ var index = require ("./index.js")
 
 console.log(index.name)
 
+<<<<<<< HEAD
 
+=======
+if (name === "Susan") {console.log("The name is correct")}
+else {
+  console.log("Expected the name to be Susan")
+}
+>>>>>>> 41ca36d0ce68b1ce9698c0c3f1b3f8206f35225c
